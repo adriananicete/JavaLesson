1678ad3964b9a2dd434bdf278ad1e64cbf869b97
Adrian Anicete <anicete.ian@gmail.com>
2026-05-11 14:03:04 +0800

    feat: learning java fundamentals

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,38 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        double width = 0;
-        double height = 0;
-        double area = 0;
-
+    static void main() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the width: ");
-        width = scanner.nextDouble();
+        String name;
+        int age;
+        String city;
+        String country;
+        String programmingLanguage;
+        String hobby;
 
-        System.out.print("Enter the height: ");
-        height = scanner.nextDouble();
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
 
-        area = width * height;
-        System.out.println("The area is: " + area + "cm²");
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter your city: ");
+        city = scanner.nextLine();
+
+        System.out.print("Enter your country: ");
+        country = scanner.nextLine();
+
+        System.out.println("Enter your programming language: ");
+        programmingLanguage = scanner.nextLine();
+
+        System.out.println("Enter your hobby: ");
+        hobby = scanner.nextLine();
+
+        System.out.println("Hello, I'am " + name + ", " + age + " years old.");
+        System.out.println("I lived in " + city + ", " + country + ".");
+        System.out.println("I love " + hobby + " and my programming language that I use is " + programmingLanguage);
+
 
         scanner.close();
     }
