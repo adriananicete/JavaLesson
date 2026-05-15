5e2be7a837ee3978fe7f813549107f41db510342
Adrian Anicete <anicete.ian@gmail.com>
2026-05-15 15:27:06 +0800

    lesson: enhance switch statement

---
+++ src/enhanceSwitch/EnhanceSwitch.java
@@ -0,0 +1,20 @@
+package enhanceSwitch;
+import java.util.Scanner;
+public class EnhanceSwitch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String day;
+
+        System.out.print("Enter your day: ");
+        day = scanner.nextLine().toLowerCase();
+
+        switch (day) {
+            case "monday","tuesday","wednesday","thursday","friday" -> System.out.println("It is a weekday!");
+            case "saturday","sunday" -> System.out.println("It is a weekend!");
+            default -> System.out.println(day + " is not a day!");
+        }
+
+        scanner.close();
+    }
+}
